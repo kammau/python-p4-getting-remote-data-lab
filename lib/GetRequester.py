a208cf272332d988ec4579c6774a1ee423e535b0
Kamryn Smith <programersign@gmail.com>
2023-08-02 12:30:57 -0700

    assignment complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,16 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        self.get_response_body()
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        data_info = json.loads(self.get_response_body())
+        for data in data_info:
+            data_list.append(data)
+        
+        return data_list
